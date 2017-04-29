a4d076dba108daca90243c4b23e846533f86bd17
Owner <Owner@Dad.frontier.com>
2017-04-29 15:17:50 -0400

    TCP client works but after transfer it crashes the server :(

--- src/tcptest/TCPClient.java
+++ src/tcptest/TCPClient.java
@@ -7,7 +7,7 @@ import java.net.*;
  * response. This is an adaption of the code provided by the Computer
  * Networking: A Top Down Approach book by Kurose and Ross
  * 
- * @author Chad Williams
+ * @author Adrian and Jeff
  */
 public class TCPClient extends Thread {
 
@@ -36,33 +36,35 @@ public class TCPClient extends Thread {
       System.out.println("CLIENT connected to server");
       
       //get file
-      byte[] bRecv = new byte[9447213];
-      InputStream is = clientSocket.getInputStream();
-      File f = new File("C:\\Users\\Owner\\Music\\Test\\test.m4a");
+      
+     
+      DataInputStream is = new DataInputStream(clientSocket.getInputStream());
+      byte[] bRecv = new byte[5069134];
+     // BufferedInputStream is = new BufferedInputStream(in);
+      File f = new File("C:\\Users\\Owner\\Music\\Test\\test.mp4");
       if(!f.exists())
           f.createNewFile();
       outFile = new FileOutputStream(f);
       buffOut = new BufferedOutputStream(outFile);
+      
+      is.readFully(bRecv);
+      /*
       bytesRead = is.read(bRecv, 0, bRecv.length);
+      System.out.println("Bytes Read is " + bytesRead);
       current = bytesRead;
+      int total = 0;
       int available = -1;
       while((available = is.read(bRecv))>0){
-          buffOut.write(bRecv, 0, available);
+         buffOut.write(bRecv, 0, available);
+         
+          buffOut.flush();
       }
-      /*
-      do{
-        bytesRead = is.read(bRecv, current, bRecv.length-current);
-        if(bytesRead>=0)
-            current+=bytesRead;
-        
-      }while(bytesRead > -1);
 */
-      //buffOut.write(bRecv, 0, current);
-      //DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-     
-     // buffOut.write(bRecv, 0, current);
+      buffOut.write(bRecv);
+      is.close();
       buffOut.flush();
       buffOut.close();
+      
       System.out.println("Closing Connection");
       clientSocket.close();
       System.out.println(this.getName() + " closed connection to server");
