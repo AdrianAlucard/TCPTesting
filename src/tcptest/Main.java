906cd308cbb96cb7c606f3cae04106d413323fe7
Owner <Owner@Dad.frontier.com>
2017-04-29 00:18:33 -0400

    IT'S ALIVE IT'S ALIVE

--- src/tcptest/Main.java
+++ src/tcptest/Main.java
@@ -11,18 +11,18 @@ package tcptest;
 public class Main {
 
   public static void main(String[] args) {
-    TCPServer serverThread = null;
-    //TCPClient clientThread = null;
+    //TCPServer serverThread = null;
+    TCPClient clientThread = null;
     try {
       // Start server
-      serverThread = new TCPServer("Server", 49000);
-      serverThread.start();
+      //serverThread = new TCPServer("Server", 49000);
+      //serverThread.start();
 
       // Create client
       //byte[] targetAdddress = {127, 0, 0, 1};
-      //TCPClient client1 = new TCPClient("CLIENT1", 49000);
+      TCPClient client1 = new TCPClient("CLIENT1", 49000);
       //TCPClient client2 = new TCPClient("CLIENT2", 49000);
-      //client1.start();
+      client1.start();
       //client2.start();
     } catch (Exception e) {
       e.printStackTrace();
--- src/tcptest/TCPClient.java
+++ src/tcptest/TCPClient.java
@@ -36,7 +36,7 @@ public class TCPClient extends Thread {
       System.out.println("CLIENT connected to server");
       
       //get file
-      byte[] bRecv = new byte[96000];
+      byte[] bRecv = new byte[9447213];
       InputStream is = clientSocket.getInputStream();
       File f = new File("C:\\Users\\Owner\\Music\\Test\\test.m4a");
       if(!f.exists())
@@ -45,18 +45,25 @@ public class TCPClient extends Thread {
       buffOut = new BufferedOutputStream(outFile);
       bytesRead = is.read(bRecv, 0, bRecv.length);
       current = bytesRead;
-      
+      int available = -1;
+      while((available = is.read(bRecv))>0){
+          buffOut.write(bRecv, 0, available);
+      }
+      /*
       do{
         bytesRead = is.read(bRecv, current, bRecv.length-current);
         if(bytesRead>=0)
             current+=bytesRead;
+        
       }while(bytesRead > -1);
-      
+*/
+      //buffOut.write(bRecv, 0, current);
       //DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
      
-      buffOut.write(bRecv, 0, current);
+     // buffOut.write(bRecv, 0, current);
       buffOut.flush();
-      
+      buffOut.close();
+      System.out.println("Closing Connection");
       clientSocket.close();
       System.out.println(this.getName() + " closed connection to server");
     } catch (Exception e) {
